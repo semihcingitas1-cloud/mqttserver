f4d6ec98411f0c830220465b237eddf7b5496ee5
semihcingitas1-cloud <semihcingitas1@gmail.com>
2026-04-15 16:36:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
 
   console.log(`Server is running on Port: ${PORT}.`);
 });
