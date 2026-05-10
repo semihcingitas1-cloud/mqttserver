ca614f12d8758548674b222ece6ce4d72bb1c0b2
semihcingitas1-cloud <semihcingitas1@gmail.com>
2026-05-10 17:52:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
 
-app.options('*', cors());
+app.options('/(.*)', cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
